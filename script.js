8022a8ab9221d9dd7a40c204dbf366c5c17f897a
fasaldana <fernando_s_98@hotmail.com>
2022-02-28 15:21:06 -0500

    Added javascript function to menu bar

---
+++ script.js
@@ -0,0 +1,12 @@
+const toggle = document.getElementsByClassName('checkbtn')[0]
+const navLi = document.getElementsByClassName('nav-li')[0]
+const text = document.getElementsByClassName('logo')[0]
+
+toggle.addEventListener('click', () => {
+  navLi.classList.toggle('active');
+    text.classList.toggle('disabled');
+})
+
+function myFunction(x) {
+    x.classList.toggle("fa-xmark");
+}

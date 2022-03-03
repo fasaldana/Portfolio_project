dc73eec41bcf97b249ad709fbca14244acd3397a
fasaldana <fernando_s_98@hotmail.com>
2022-03-03 01:52:01 -0500

    Fixed linters errors

--- work-cards.js
+++ work-cards.js
@@ -1,20 +1,19 @@
 const midpart = document.querySelector('.works');
 const data = [
-    {
-      name: 'Professional Art Printing Data', 
-      description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-      featuredImage: 'media/work_example.png',
-      tecnologies: ['Html', 'Bootstrap', 'Ruby on rails'],
-      liveVer: 'https://fasaldana.github.io/Portfolio_project/',
-      source: 'https://github.com/fasaldana/Portfolio_project',
-    },
+  {
+    name: 'Professional Art Printing Data',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    featuredImage: 'media/work_example.png',
+    tecnologies: ['Html', 'Bootstrap', 'Ruby on rails'],
+    liveVer: 'https://fasaldana.github.io/Portfolio_project/',
+    source: 'https://github.com/fasaldana/Portfolio_project',
+  },
+];
 
-  ];
-  
 
-  const container = document.querySelector('.work-cards');
-  for(let i=0; i<data.length; i++){
-  container.innerHTML = `
+const container = document.querySelector('.work-cards');
+for (let i = 0; i < data.length; i++) {
+    container.innerHTML = `
     <div class="box">
         <h2 class="box-title">${data[i].name}</h2>
         <p class="box-desc">${data[i].description}</p>
@@ -81,8 +80,8 @@ const data = [
 //popupWindow
 let modalContainer;
 function popupWindow() {
-  modalContainer = document.createElement('section');
-  modalContainer.innerHTML = `
+    modalContainer = document.createElement('section');
+    modalContainer.innerHTML = `
   <div class="modalContainer">
         <div class="modalHeader">
             <h3>${data[0].name}</h3>
@@ -106,8 +105,8 @@ function popupWindow() {
             </div>
         </div>
 </div>`
-;
-midpart.appendChild(modalContainer);
+        ;
+    midpart.appendChild(modalContainer);
 }
 popupWindow();
 
@@ -117,20 +116,19 @@ const popupwind = document.getElementsByClassName('modalContainer')[0];
 for (let i = 0; i < 6; i += 1) {
     const openModalButtons2 = document.getElementsByClassName('box-btn')[i];
     const closeModalButtons2 = document.getElementsByClassName('close-modal')[0];
-  
-    openModalButtons2.addEventListener('click', () => {
-      overlay.classList.add('opened');
-      popupwind.classList.add('opened');
-    });
-  
-    overlay.addEventListener('click', () => {
-      overlay.classList.remove('opened');
-      popupwind.classList.remove('opened');
-    });
-  
-    closeModalButtons2.addEventListener('click', () => {
-      overlay.classList.remove('opened');
-      popupwind.classList.remove('opened');
-    });
-  }
 
+    openModalButtons2.addEventListener('click', () => {
+        overlay.classList.add('opened');
+        popupwind.classList.add('opened');
+    });
+
+    overlay.addEventListener('click', () => {
+        overlay.classList.remove('opened');
+        popupwind.classList.remove('opened');
+    });
+
+    closeModalButtons2.addEventListener('click', () => {
+        overlay.classList.remove('opened');
+        popupwind.classList.remove('opened');
+    });
+}

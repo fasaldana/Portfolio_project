f8469681d246d34aa6e3c5f8be144c6b5b9a0549
fasaldana <fernando_s_98@hotmail.com>
2025-02-23 19:20:57 -0500

    Fix bugs

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-const toggle = document.getElementsByClassName('checkbtn')[0];
-const navLi = document.getElementsByClassName('nav-li')[0];
-const menuBtn = document.querySelectorAll('.menu-btn');
+const toggle = document.getElementsByClassName("checkbtn")[0];
+const navLi = document.getElementsByClassName("nav-li")[0];
+const menuBtn = document.querySelectorAll(".menu-btn");
 
 menuBtn.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    navLi.classList.toggle('active');
-    document.body.classList.toggle('stop-scrolling');
+  btn.addEventListener("click", () => {
+    navLi.classList.toggle("active");
   });
 });
 
-toggle.addEventListener('click', () => {
-  navLi.classList.toggle('active');
-  document.body.classList.toggle('stop-scrolling');
+toggle.addEventListener("click", () => {
+  navLi.classList.toggle("active");
 });
 
 function myFunction(x) {
   // eslint-disable-line no-unused-vars
-  x.classList.toggle('fa-xmark');
+  x.classList.toggle("fa-xmark");
 }
 
 //Get the button:
-mybutton = document.getElementById('myBtn');
+mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
@@ -29,12 +27,12 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = 'block';
-    document.getElementById('nav').style.top = '-50px';
-    document.getElementById('nav').style.transition = '0.5s';
+    mybutton.style.display = "block";
+    document.getElementById("nav").style.top = "-50px";
+    document.getElementById("nav").style.transition = "0.5s";
   } else {
-    mybutton.style.display = 'none';
-    document.getElementById('nav').style.top = '0';
+    mybutton.style.display = "none";
+    document.getElementById("nav").style.top = "0";
   }
 }
 
@@ -45,7 +43,7 @@ function topFunction() {
 }
 
 function reveal() {
-  var reveals = document.querySelectorAll('.reveal');
+  var reveals = document.querySelectorAll(".reveal");
 
   for (var i = 0; i < reveals.length; i++) {
     var windowheight = window.innerHeight;
@@ -53,11 +51,11 @@ function reveal() {
     var revealpoint = 150;
 
     if (revealtop < windowheight - revealpoint) {
-      reveals[i].classList.add('active');
+      reveals[i].classList.add("active");
     } else {
-      reveals[i].classList.remove('active');
+      reveals[i].classList.remove("active");
     }
   }
 }
 
-window.addEventListener('scroll', reveal);
+window.addEventListener("scroll", reveal);
